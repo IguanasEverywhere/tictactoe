0519edbab6a571d9f5d38e74d01d88c08ada6ac2
Scott Schwab <schwabpiano@gmail.com>
2022-01-29 15:02:37 -0600

    Add controlGame function that checks for winning space combinations.

--- index.js
+++ index.js
@@ -1,19 +1,41 @@
 const Player = (marker, markedSpaces) => {
 
     const markBox = (square) => {
-        playerTwo.markedSpaces.push(square.id);
-        console.log(square.id);
-        console.log(markedSpaces);
+        if (!playerTwo.markedSpaces.includes(square.id)) {
+            playerTwo.markedSpaces.push(square.id);
+            // console.log(square.id);
+            // console.log(markedSpaces);
+            square.textContent = playerTwo.marker; //placeholder for now
+            controlGame(markedSpaces);
+        }
+
+
     }
-    return {marker, markedSpaces, markBox};
+    return { marker, markedSpaces, markBox };
 }
 
 const playerOne = Player("X", []);
 const playerTwo = Player("O", []);
 
+const controlGame = (markedSpaces) => {
+
+    console.log(markedSpaces);
+    if (markedSpaces.includes('0') && markedSpaces.includes('1') && markedSpaces.includes('2') ||
+        markedSpaces.includes('3') && markedSpaces.includes('4') && markedSpaces.includes('5') ||
+        markedSpaces.includes('6') && markedSpaces.includes('7') && markedSpaces.includes('8') ||
+        markedSpaces.includes('0') && markedSpaces.includes('3') && markedSpaces.includes('6') ||
+        markedSpaces.includes('1') && markedSpaces.includes('4') && markedSpaces.includes('7') ||
+        markedSpaces.includes('2') && markedSpaces.includes('5') && markedSpaces.includes('8') ||
+        markedSpaces.includes('0') && markedSpaces.includes('4') && markedSpaces.includes('8') ||
+        markedSpaces.includes('2') && markedSpaces.includes('4') && markedSpaces.includes('6')) {
+        console.log("winner!");
+    } 
+}
+
+
 
 const gameBoard = (() => {
-    let spaces = [0,1,2,3,4,5,6,7,8];
+    let spaces = [0, 1, 2, 3, 4, 5, 6, 7, 8];
     let gameArea = document.getElementById("gameArea");
 
     let num = 0;
@@ -26,15 +48,14 @@ const gameBoard = (() => {
             gameArea.appendChild(square);
             num++;
 
-           
+
             square.addEventListener("click", () => {
                 playerTwo.markBox(square);
-                square.textContent = playerTwo.marker; //placeholder for now
             });
         });
     }
 
-    return {renderBoard};
+    return { renderBoard };
 })();
 
 gameBoard.renderBoard();
