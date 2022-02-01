e802051d13b503c75cb23d2ac2e40eb2842b2153
Scott Schwab <schwabpiano@gmail.com>
2022-02-01 16:04:05 -0600

    change order of Player and RenderBoard objects, change method of passing arrays from to the other

--- index.js
+++ index.js
@@ -1,13 +1,3 @@
-const Player = (marking) => {
-    let playerArray = [];
-
-    return { marking };
-}
-
-const playerOne = Player("X");
-const playerTwo = Player("O");
-
-
 const gameBoard = (() => {
     let spaces = [0, 1, 2, 3, 4, 5, 6, 7, 8];
     let gameArea = document.getElementById("gameArea");
@@ -24,22 +14,34 @@ const gameBoard = (() => {
 
 
             square.addEventListener("click", () => {
-                square.textContent = "X";
+                // square.textContent = "X";
                 spaces[square.id] = "X";
+                playerOne.markBoard(spaces);
             });
 
         });
     }
 
-    return { renderBoard, spaces };
+    return { renderBoard };
 })();
 
 gameBoard.renderBoard();
 
 
-const controlGame = (() => {
-    console.log(gameBoard.spaces);
-    playerOne.playerArray = gameBoard.spaces;
+const Player = (marking) => {
+    let playerArray = [];
+    const board = gameBoard.renderBoard;
 
+    const markBoard = (spaces) => {
+        //    board.square.textContent = "X";
+        //     board.spaces[square.id] = "X";
+        playerArray = spaces;
+        console.log(playerArray);
 
-})();
+    }
+
+    return { markBoard };
+}
+
+const playerOne = Player("X");
+const playerTwo = Player("O");
